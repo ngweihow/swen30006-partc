a677bc4686e646b01dd7d3320ffaedfd62633ddf
OpticGenius <opticgenius@hotmail.com>
2018-05-21 20:39:26 +1000

    Made some changes to Graph and Node classes

--- src/mycontroller/data_structure/Graph.java
+++ src/mycontroller/data_structure/Graph.java
@@ -2,24 +2,12 @@ package mycontroller.data_structure;
 
 import tiles.MapTile;
 import utilities.Coordinate;
-import world.Car;
-import controller.AIController;
 import java.util.HashMap;
-
-import java.util.HashMap;
-import java.util.Iterator;
 
 public class Graph {
-
-    public Graph( HashMap<Coordinate, MapTile> map){
-
-
-
+    private HashMap<Coordinate, MapTile> map;
+    
+    public Graph(HashMap<Coordinate, MapTile> map){
+        this.map = map;
     }
-
-
-
-
-
-
 }
