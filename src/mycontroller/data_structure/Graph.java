31f37683ab5abab6744a613284fb493103662a5c
OpticGenius <opticgenius@hotmail.com>
2018-05-22 14:53:16 +1000

    Changed constants in graph

--- src/mycontroller/data_structure/Graph.java
+++ src/mycontroller/data_structure/Graph.java
@@ -8,13 +8,16 @@ import java.util.HashMap;
 
 public class Graph {
 
-    private final int WALL=10000;
-    private final int FINISH=1;
-    private final int START=10;
-    private final int ROAD=10;
-    private final int TRAP_BAD=1000;
-    private final int TRAP_GOOD=5;
-    private final int TILE_NOT_FOUND=-1;
+    private static final int WALL=10000;
+    private static final int FINISH=1;
+    private static final int START=10;
+    private static final int ROAD=10;
+    private static final int LAVA_TRAP=1000;
+    private static final int HEALTH_TRAP=5;
+    private static final int TILE_NOT_FOUND=-1;
+
+    private static final String LAVA = "lava";
+    private static final String HEALTH = "health";
 
     // Map of world
     private HashMap<Coordinate, MapTile> map;
@@ -51,28 +54,19 @@ public class Graph {
 
         if (tile.isType(MapTile.Type.TRAP)) {
             TrapTile trapTile = (TrapTile) tile;
-            if (trapTile.getTrap().equals("health")) {
-                return TRAP_GOOD;
-
-            }
-            if (trapTile.getTrap().equals("lava")) {
-                return TRAP_BAD;
-
+            if (trapTile.getTrap().equals(HEALTH)) {
+                return HEALTH_TRAP;
+            } else if (trapTile.getTrap().equals(LAVA)) {
+                return LAVA_TRAP;
             }
 
-        }
-        if (tile.isType(MapTile.Type.WALL)) {
+        } else if (tile.isType(MapTile.Type.WALL)) {
             return WALL;
-        }
-
-        if (tile.isType(MapTile.Type.FINISH)) {
+        } else if (tile.isType(MapTile.Type.FINISH)) {
             return FINISH;
-        }
-        if (tile.isType(MapTile.Type.START)) {
+        } else if (tile.isType(MapTile.Type.START)) {
             return START;
-
-        }
-        if (tile.isType(MapTile.Type.ROAD)) {
+        } else if (tile.isType(MapTile.Type.ROAD)) {
             return ROAD;
         }
 
