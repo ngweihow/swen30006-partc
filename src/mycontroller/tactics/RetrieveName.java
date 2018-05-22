2a5476d6f2aa1c20f57e1eb1f4fa455b5b5e3421
rene <ngweihow.r@gmail.com>
2018-05-22 13:47:37 +1000

    Added tacticfactory

--- src/mycontroller/tactics/RetrieveName.java
+++ src/mycontroller/tactics/RetrieveName.java
@@ -1,14 +1,24 @@
 package mycontroller.tactics;
 
+import mycontroller.data_structure.Node;
 import tiles.MapTile;
 import utilities.Coordinate;
+
 
 import java.util.HashMap;
 
 public class RetrieveName implements ITraversalTactic {
 
+    public RetrieveName() {
 
-    public boolean tacticalBehaviour(HashMap<Coordinate, MapTile> currentView, Coordinate coordinate) {
+    }
+
+    public boolean tacticalBehaviour(HashMap<Node, MapTile> currentView, Coordinate coordinate) {
         return false;
+    }
+
+    @Override
+    public void travel() {
+
     }
 }
