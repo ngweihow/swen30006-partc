dbfeacb45bb4046063375e8631f28e8a4657d801
rene <ngweihow.r@gmail.com>
2018-05-25 16:50:58 +1000

    Added comments in exit

--- src/mycontroller/strategies/Exit.java
+++ src/mycontroller/strategies/Exit.java
@@ -10,16 +10,22 @@ import java.util.Map;
 import java.util.Stack;
 
 public class Exit implements ITraversalStrategy {
+
+    // Initialising the Node and referral graph
     private Map<Node, MapTile> graph;
     private Node destination;
+
 
     public Exit(Map<Node, MapTile> graph) {
         this.graph = graph;
         this.destination = getDestination(graph);
-
-
     }
 
+    /**
+     * Get the Destination for the finish node.
+     * @param graph HashMap reference for getting tiles.
+     * @return Destination Node.
+     */
     public Node getDestination(Map<Node, MapTile> graph) {
         for (Map.Entry<Node, MapTile> entry: graph.entrySet()) {
             if (entry.getValue().isType(MapTile.Type.FINISH)) {
